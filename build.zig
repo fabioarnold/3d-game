8fd6f69e727544b368a6fcb5c5d22f47332a3338
Fabio Arnold <fabio@fabioarnold.de>
2024-09-27 11:10:09 +0200

    Update to Zig 0.13.0

--- build.zig
+++ build.zig
@@ -7,19 +7,19 @@ pub fn build(b: *std.Build) void {
     const zgltf_dep = b.dependency("zgltf", .{ .target = target, .optimize = optimize });
     const exe = b.addExecutable(.{
         .name = "main",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
     exe.root_module.addImport("zalgebra", zalgebra_dep.module("zalgebra"));
     exe.root_module.addImport("zgltf", zgltf_dep.module("zgltf"));
-    exe.root_module.addAnonymousImport("assets", .{ .root_source_file = .{ .path = "Content/assets.zig" } });
+    exe.root_module.addAnonymousImport("assets", .{ .root_source_file = b.path("Content/assets.zig") });
     exe.rdynamic = true;
     exe.entry = .disabled;
     b.installArtifact(exe);
 
     const tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/tests.zig" },
+        .root_source_file = b.path("src/tests.zig"),
     });
     tests.root_module.addImport("zalgebra", zalgebra_dep.module("zalgebra"));
     const run_tests = b.addRunArtifact(tests);
